18f4233de55110684eeba851b078bddf4ece52b9
martinyyang <martinyyang@users.noreply.github.com>
2026-08-02 11:14:50 +0000

    deploy: 2954a286546e09200404df4cb0dc5e2477abdf2b

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "3813b07cf3e77ebb400d8be51264f39c",
-"index.html": "96802f7b3eb29df15f9753a6a16da14d",
-"/": "96802f7b3eb29df15f9753a6a16da14d",
-"main.dart.js": "4bac2bd889958df643ac45e302abbc9e",
+const RESOURCES = {"flutter_bootstrap.js": "c1a0f8383453eb6a36551234b6016c75",
+"index.html": "1ff30177e2f73ec62a9eeb4a4148b458",
+"/": "1ff30177e2f73ec62a9eeb4a4148b458",
+"main.dart.js": "832e6ccecffb9ed1ab1a389721089846",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "canvaskit/skwasm.wasm": "9f0c0c02b82a910d12ce0543ec130e60",
 "canvaskit/skwasm.js": "694fda5704053957c2594de355805228",
