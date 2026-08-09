9ba66d5831041ca0b6982ce477ae143c9430fec6
martinyyang <martinyyang@users.noreply.github.com>
2026-08-09 11:09:50 +0000

    deploy: 420863af7dedce468083d43d07d7a31f289bd437

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2073660931"
+    serviceWorkerVersion: "3389976474"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,9 +3,9 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "c46aa34e9638b2c95d0bf2d91d0f6759",
-"index.html": "c80c0df804ba5ce9f91dd230a9455d05",
-"/": "c80c0df804ba5ce9f91dd230a9455d05",
+const RESOURCES = {"flutter_bootstrap.js": "637973885725def6ce600ed71decd664",
+"index.html": "86693f96f3be2b05254d85cfd4e582dd",
+"/": "86693f96f3be2b05254d85cfd4e582dd",
 "main.dart.js": "15ae6beab1c4d22466f1ce000adb2cce",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "test-env.html": "831fefc00343040ec7ce3b4b7301f616",
